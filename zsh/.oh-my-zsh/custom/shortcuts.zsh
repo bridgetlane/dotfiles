ec22ead7411746d8896a566145ea80ecf718a1d8
Bridget Lane <bnlane@gannett.com>
2015-08-08 18:12:09 -0400

    nyan and some plugins

--- zsh/.oh-my-zsh/custom/shortcuts.zsh
+++ zsh/.oh-my-zsh/custom/shortcuts.zsh
@@ -6,3 +6,5 @@ alias openzeus="ssh blane3@zeus.ite.gmu.edu"
 alias cb="cd ~/chef-repo/cookbooks"
 alias db="cd ~/chef-repo/data_bags"
 alias ro="cd ~/chef-repo/roles"
+alias nyan="nc -v nyancat.dakko.us 23" # via oh-my-zsh nyan plugin
+
