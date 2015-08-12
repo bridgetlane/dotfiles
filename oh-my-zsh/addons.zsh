d1bf3f4cecb1eae6801cf0a47166e5067a9782e2
Bridget Lane <bnlane@gannett.com>
2015-08-12 16:09:31 -0400

    virtualenv handled by zsh plugin

--- oh-my-zsh/addons.zsh
+++ oh-my-zsh/addons.zsh
@@ -3,9 +3,3 @@
 export VISUAL=vim
 export EDITOR="$VISUAL"
 
-##############################################################################################
-# virtualenvwrapper startup
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Devel
-source /usr/local/bin/virtualenvwrapper.sh
-
